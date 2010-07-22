e0d5467a60475a5bff6a2f07c8158ad43622b4ab
LongTail Video <LongTail Video@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-22 22:58:52 +0000

    - new layout for playlist manager
    git-svn-id: https://plugins.svn.wordpress.org/jw-player-plugin-for-wordpress/trunk@266121 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- media/EmbedManager.php
+++ media/EmbedManager.php
@@ -1,6 +1,7 @@
 <?php
 
 global $jw_query;
+global $p_items;
 
 /**
  * @file Definition of the JW Playlist Manager.
@@ -17,7 +18,7 @@ global $jw_query;
  * @param undefined $errors List of any errors encountered.
  */
 function media_jwplayer_insert_form($errors) {
-	global $redir_tab, $type, $jw_query;
+	global $redir_tab, $type, $jw_query, $p_items;
 
   $args = array(
     'post_parent' => null,
@@ -28,7 +29,6 @@ function media_jwplayer_insert_form($errors) {
     'orderby' => 'menu_order ASC, ID', 'order' => 'DESC'
   );
   $jw_query = new WP_Query($args);
-//  echo print_r($jw_query) . "<br/>";
 
 	$redir_tab = 'jwplayer';
 	media_upload_header();
@@ -41,6 +41,7 @@ function media_jwplayer_insert_form($errors) {
     $post_title = $_POST[LONGTAIL_KEY . "playlist_name"];
     $playlist_items = array();
     $attachments = $_POST["attachments"];
+    $playlist_attachments = $_POST["playlist_attachments"];
     foreach ($attachments as $key => $attachment) {
       if(isset($attachment["enabled"])) {
         $playlist_items[] = $key;
@@ -67,6 +68,13 @@ function media_jwplayer_insert_form($errors) {
   if (!isset($current_playlist)) {
     $current_playlist = isset($_POST[LONGTAIL_KEY . "playlist_select"]) ? $_POST[LONGTAIL_KEY . "playlist_select"] : $playlists[0]->ID;
   }
+
+  $p_items = isset($_POST["playlist_items"]) ? json_decode(str_replace("\\", "", $_POST["playlist_items"])) : explode(",", get_post_meta($current_playlist, LONGTAIL_KEY. "playlist_items", true));
+
+  echo print_r($p_items);
+
+  echo '<link rel="stylesheet" href="'. WP_PLUGIN_URL . '/' . plugin_basename( dirname(dirname(__FILE__)) ).'/' . 'css/playlist.css" type="text/css" media="print, projection, screen" />'."\n";
+  echo '<script type=text/javascript src="' . WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ).'/' . 'js/playlist.js"></script>'."\n"
 ?>
 
 <script type="text/javascript">
@@ -77,6 +85,26 @@ function media_jwplayer_insert_form($errors) {
       preloaded.each(function(){prepareMediaItem({id:this.id.replace(/[^0-9]/g, '')},'');});
       updateMediaForm();
     }
+    var playlistPreloaded = $(".playlist-item.preloaded");
+    if ( playlistPreloaded.length > 0 ) {
+      playlistPreloaded.each(function(){preparePlaylistItem({id:this.id.replace(/[^0-9]/g, '')},'');});
+      updatePlaylistForm();
+    }
+    $('#playlist-items').sortable( {
+			items: 'div.playlist-item',
+			placeholder: 'sorthelper',
+			axis: 'y',
+			distance: 2,
+			handle: 'div.filename',
+			stop: function(e, ui) {
+				// When an update has occurred, adjust the order for each item
+				var all = $('#playlist-items').sortable('toArray'), len = all.length;
+				$.each(all, function(i, id) {
+					var order = desc ? (len - i) : (1 + i);
+					$('#' + id + ' .menu_order input').val(order);
+				});
+			}
+		} );
   });
   -->
 
@@ -109,6 +137,19 @@ function media_jwplayer_insert_form($errors) {
     }
     return true;
   }
+
+  function updatePlaylist(object) {
+    var item_list = document.getElementById("playlist_items");
+    var p_items = eval('(' + item_list.value + ')');
+    var attachment_id = object.id.replace("attachments[", "").replace("][enabled]", "");
+    if (object.checked === true) {
+      p_items.push(attachment_id);
+    } else {
+      p_items.splice(attachment_id, 1);
+    }
+    alert(p_items);
+    item_list.value = dump(p_items);
+  }
 </script>
 
 <form enctype="multipart/form-data" method="post" action="<?php echo esc_attr($form_action_url); ?>" class="media-upload-form validate" id="playlist-form">
@@ -125,13 +166,15 @@ function media_jwplayer_insert_form($errors) {
         'current' => $_GET['paged'] ? $_GET['paged'] : 1
       ));
 
-      if ( $page_links ) {
-        echo "<div class='tablenav-pages'>$page_links</div>";
-      }
-    ?>
+      if ( $page_links ) { ?>
+        <div class='tablenav-pages'>
+          <span style="font-size: 13px;"><?php _e("Available Media:"); ?></span>
+          <?php echo $page_links; ?>
+        </div>
+      <?php }?>
     <div class="alignleft actions">
       <div class="hide-if-no-js">
-        <?php _e("Select Playlist:"); ?>
+        <?php _e("Playlist:"); ?>
         <select onchange="this.form.submit()" id="<?php echo LONGTAIL_KEY . "playlist_select"; ?>" name="<?php echo LONGTAIL_KEY . "playlist_select"; ?>">
           <?php foreach ($playlists as $playlist) { ?>
             <option value="<?php echo $playlist->ID; ?>" <?php selected($playlist->ID, $current_playlist); ?>>
@@ -139,19 +182,26 @@ function media_jwplayer_insert_form($errors) {
             </option>
           <?php } ?>
         </select>
-        <input type="submit" class="button savebutton" style="display:none;" name="save" id="save-all" value="<?php esc_attr_e( 'Save all changes' ); ?>" />
-        <input type="submit" class="button savebutton" style="display:none;" name="delete" id="delete-all" value="<?php esc_attr_e( 'Delete Playlist' ); ?>" onclick="return deletePlaylistHandler()" />
+        <input type="submit" class="button savebutton" style="display:none;" name="save" id="save-all" value="<?php esc_attr_e( 'Save' ); ?>" />
+        <input type="submit" class="button savebutton" style="display:none;" name="delete" id="delete-all" value="<?php esc_attr_e( 'Delete' ); ?>" onclick="return deletePlaylistHandler()" />
         <input type="hidden" name="post_id" id="post_id" value="<?php echo (int) $post_id; ?>" />
         <input type="hidden" name="type" value="<?php echo esc_attr( $GLOBALS['type'] ); ?>" />
         <input type="hidden" name="tab" value="<?php echo esc_attr( $GLOBALS['tab'] ); ?>" />
+        <input type="hidden" id="playlist_items" name="playlist_items" value='<?php echo json_encode($p_items); ?>' />
       </div>
     </div>
   </div>
 
-  <div id="media-items">
+  <div id="playlist-items" style="width: 300px; float: left;" class="ui-sortable">
+    <?php echo get_jw_playlist_items($post_id, $errors, $current_playlist); ?>
+  </div>
+
+  <div id="media-items" style="width: 300px; float: right;">
     <?php add_filter('attachment_fields_to_edit', 'media_post_single_attachment_fields_to_edit', 10, 2); ?>
     <?php echo get_playlist_items($post_id, $errors, $current_playlist); ?>
   </div>
+
+  <div class="clear"></div>
 
   <p class="ml-submit">
     <?php _e("New Playlist:"); ?>
@@ -176,6 +226,27 @@ function media_jwplayer_insert_form($errors) {
 <?php
 }
 
+function get_jw_playlist_items($post_id, $errors, $current_playlist) {
+  global $p_items;
+
+  $playlist_items = get_children(array( 'post_parent' => null, 'post_type' => 'attachment', 'orderby' => 'menu_order ASC, ID', 'order' => 'DESC'));
+
+  $attachments = array();
+  foreach ($p_items as $p_item) {
+    $attachments[$p_item] = $playlist_items[$p_item];
+  }
+
+  $output = '';
+	foreach ( (array) $attachments as $id => $attachment ) {
+		if ( $attachment->post_status == 'trash' )
+			continue;
+		if ( $item = get_jw_playlist_item( $id, array( 'errors' => isset($errors[$id]) ? $errors[$id] : null), $current_playlist))
+			$output .= "\n<div id='playlist-item-$id' class='playlist-item child-of-$attachment->post_parent preloaded'><div class='progress'><div class='bar'></div></div><div id='playlist-upload-error-$id'></div><div class='filename'></div>$item\n</div>";
+	}
+
+	return $output;
+}
+
 /**
  * Get the list of playlist items to be displayed.  This list consists of
  * all media attachments in the system.
@@ -186,21 +257,15 @@ function media_jwplayer_insert_form($errors) {
  */
 function get_playlist_items($post_id, $errors, $current_playlist) {
   global $jw_query;
+
   $playlist_items = array();
-//  $playlist_items = get_children( array( 'post_parent' => null, 'post_type' => 'attachment', 'orderby' => 'menu_order ASC, ID', 'order' => 'DESC') );
   while($jw_query->have_posts()) {
     $post = $jw_query->next_post();
     $playlist_items[$post->ID] = $post;
   }
 
-//  $playlist_items = $jw_query->get_posts();
-//  echo "Count: " . count($playlist_items);
-
   $playlist_item_ids = explode(",", get_post_meta($current_playlist, LONGTAIL_KEY. "playlist_items", true));
   $attachments = array();
-//  foreach ($playlist_item_ids as $playlist_item_id) {
-//    $attachments[$playlist_item_id] = $playlist_items[$playlist_item_id];
-//  }
 
   foreach ($playlist_items as $playlist_item) {
     $attachments[$playlist_item->ID] = $playlist_item;
@@ -288,7 +353,7 @@ function get_playlist_item($attachment_id, $args = null, $current_playlist) {
 		if ( 'menu_order' == $key ) {
 			if ( true ) {
 				$order = '<div class="menu_order">';
-        $order .= '<input class="menu_order_input" type="checkbox" id="attachments['.$attachment_id.'][enabled]" name="attachments['.$attachment_id.'][enabled]" value="'.$val['value'].'"'.$checked.' /></div>';
+        $order .= '<input class="menu_order_input" type="checkbox" id="attachments['.$attachment_id.'][enabled]" name="attachments['.$attachment_id.'][enabled]" value="'.$val['value'].'"'.$checked.' onchange="updatePlaylist(this);" /></div>';
       } else
 				$order = '<input type="hidden" name="attachments['.$attachment_id.'][menu_order]" value="'.$val['value'].'" />';
 
@@ -316,6 +381,217 @@ function get_playlist_item($attachment_id, $args = null, $current_playlist) {
 	$display_title
 	<table class='slidetoggle describe $class'>
 		<thead class='media-item-info' id='media-head-$post->ID'>
+		<tr>
+			<td class='A1B1' id='thumbnail-head-$post->ID' rowspan='5'><img class='thumbnail' src='$thumb_url' alt='' /></td>
+			<td><strong>" . __('File name:') . "</strong> $filename</td>
+		</tr>
+		<tr><td><strong>" . __('File type:') . "</strong> $post->post_mime_type</td></tr>
+		<tr><td><strong>" . __('Upload date:') . "</strong> " . mysql2date( get_option('date_format'), $post->post_date ) . "</td></tr>\n";
+
+	if ( !empty($media_dims) )
+		$item .= "<tr><td><strong>" . __('Dimensions:') . "</strong> $media_dims</td></tr>\n";
+
+	$item .= "
+		<tr><td class='A1B1'>$image_edit_button</td></tr>
+		</thead>
+		<tbody>
+		<tr><td colspan='2' class='imgedit-response' id='imgedit-response-$post->ID'></td></tr>
+		<tr><td style='display:none' colspan='2' class='image-editor' id='image-editor-$post->ID'></td></tr>\n";
+
+	$defaults = array(
+		'input'      => 'text',
+		'required'   => false,
+		'value'      => '',
+		'extra_rows' => array(),
+	);
+
+	if ( $send )
+		$send = "<input type='submit' class='button' name='send[$attachment_id]' value='" . esc_attr__( 'Insert into Post' ) . "' />";
+	if ( $delete && current_user_can('delete_post', $attachment_id) ) {
+		if ( !EMPTY_TRASH_DAYS ) {
+			$delete = "<a href=\"" . wp_nonce_url("post.php?action=delete&amp;post=$attachment_id", 'delete-post_' . $attachment_id) . "\" id=\"del[$attachment_id]\" class=\"delete\">" . __('Delete Permanently') . "</a>";
+		} elseif ( !MEDIA_TRASH ) {
+			$delete = "<a href=\"#\" class=\"del-link\" onclick=\"document.getElementById('del_attachment_$attachment_id').style.display='block';return false;\">" . __('Delete') . "</a> <div id=\"del_attachment_$attachment_id\" class=\"del-attachment\" style=\"display:none;\">" . sprintf(__("You are about to delete <strong>%s</strong>."), $filename) . " <a href=\"" . wp_nonce_url("post.php?action=delete&amp;post=$attachment_id", 'delete-post_' . $attachment_id) . "\" id=\"del[$attachment_id]\" class=\"button\">" . __('Continue') . "</a> <a href=\"#\" class=\"button\" onclick=\"this.parentNode.style.display='none';return false;\">" . __('Cancel') . "</a></div>";
+		} else {
+			$delete = "<a href=\"" . wp_nonce_url("post.php?action=trash&amp;post=$attachment_id", 'trash-post_' . $attachment_id) . "\" id=\"del[$attachment_id]\" class=\"delete\">" . __('Move to Trash') . "</a> <a href=\"" . wp_nonce_url("post.php?action=untrash&amp;post=$attachment_id", 'untrash-post_' . $attachment_id) . "\" id=\"undo[$attachment_id]\" class=\"undo hidden\">" . __('Undo') . "</a>";
+		}
+	} else {
+		$delete = '';
+	}
+
+	$thumbnail = '';
+	$calling_post_id = 0;
+	if ( isset( $_GET['post_id'] ) )
+		$calling_post_id = $_GET['post_id'];
+	elseif ( isset( $_POST ) && count( $_POST ) ) // Like for async-upload where $_GET['post_id'] isn't set
+		$calling_post_id = $post->post_parent;
+	if ( 'image' == $type && $calling_post_id && current_theme_supports( 'post-thumbnails', get_post_type( $calling_post_id ) ) && get_post_thumbnail_id( $calling_post_id ) != $attachment_id )
+		$thumbnail = "<a class='wp-post-thumbnail' id='wp-post-thumbnail-" . $attachment_id . "' href='#' onclick='WPSetAsThumbnail(\"$attachment_id\");return false;'>" . esc_html__( "Use as thumbnail" ) . "</a>";
+
+	if ( ( $send || $thumbnail || $delete ) && !isset($form_fields['buttons']) )
+		$form_fields['buttons'] = array('tr' => "\t\t<tr class='submit'><td></td><td class='savesend'>$send $thumbnail $delete</td></tr>\n");
+
+	$hidden_fields = array();
+
+	foreach ( $form_fields as $id => $field ) {
+		if ( $id{0} == '_' )
+			continue;
+
+		if ( !empty($field['tr']) ) {
+			$item .= $field['tr'];
+			continue;
+		}
+
+		$field = array_merge($defaults, $field);
+		$name = "attachments[$attachment_id][$id]";
+
+		if ( $field['input'] == 'hidden' ) {
+			$hidden_fields[$name] = $field['value'];
+			continue;
+		}
+
+		$required = $field['required'] ? '<abbr title="required" class="required">*</abbr>' : '';
+		$aria_required = $field['required'] ? " aria-required='true' " : '';
+		$class  = $id;
+		$class .= $field['required'] ? ' form-required' : '';
+
+		$item .= "\t\t<tr class='$class'>\n\t\t\t<th valign='top' scope='row' class='label'><label for='$name'><span class='alignleft'>{$field['label']}</span><span class='alignright'>$required</span><br class='clear' /></label></th>\n\t\t\t<td class='field'>";
+		if ( !empty($field[$field['input']]) )
+			$item .= $field[$field['input']];
+		elseif ( $field['input'] == 'textarea' ) {
+			$item .= "<textarea type='text' id='$name' name='$name'" . $aria_required . ">" . esc_html( $field['value'] ) . "</textarea>";
+		} else {
+			$item .= "<input type='text' class='text' id='$name' name='$name' value='" . esc_attr( $field['value'] ) . "'" . $aria_required . "/>";
+		}
+		if ( !empty($field['helps']) )
+			$item .= "<p class='help'>" . join( "</p>\n<p class='help'>", array_unique((array) $field['helps']) ) . '</p>';
+		$item .= "</td>\n\t\t</tr>\n";
+
+		$extra_rows = array();
+
+		if ( !empty($field['errors']) )
+			foreach ( array_unique((array) $field['errors']) as $error )
+				$extra_rows['error'][] = $error;
+
+		if ( !empty($field['extra_rows']) )
+			foreach ( $field['extra_rows'] as $class => $rows )
+				foreach ( (array) $rows as $html )
+					$extra_rows[$class][] = $html;
+
+		foreach ( $extra_rows as $class => $rows )
+			foreach ( $rows as $html )
+				$item .= "\t\t<tr><td></td><td class='$class'>$html</td></tr>\n";
+	}
+
+	if ( !empty($form_fields['_final']) )
+		$item .= "\t\t<tr class='final'><td colspan='2'>{$form_fields['_final']}</td></tr>\n";
+	$item .= "\t</tbody>\n";
+	$item .= "\t</table>\n";
+
+	foreach ( $hidden_fields as $name => $value )
+		$item .= "\t<input type='hidden' name='$name' id='$name' value='" . esc_attr( $value ) . "' />\n";
+
+	if ( $post->post_parent < 1 && isset($_REQUEST['post_id']) ) {
+		$parent = (int) $_REQUEST['post_id'];
+		$parent_name = "attachments[$attachment_id][post_parent]";
+
+		$item .= "\t<input type='hidden' name='$parent_name' id='$parent_name' value='" . $parent . "' />\n";
+	}
+
+	return $item;
+}
+
+function get_jw_playlist_item($attachment_id, $args = null, $current_playlist) {
+	global $redir_tab;
+
+	if ( ( $attachment_id = intval($attachment_id) ) && $thumb_url = get_attachment_icon_src( $attachment_id ) )
+		$thumb_url = $thumb_url[0];
+	else
+		return false;
+
+	$default_args = array( 'errors' => null, 'send' => true, 'delete' => true, 'toggle' => true, 'show_title' => true );
+	$args = wp_parse_args( $args, $default_args );
+	extract( $args, EXTR_SKIP );
+
+	$toggle_on = __('Show');
+	$toggle_off = __('Hide');
+
+	$post = get_post($attachment_id);
+
+	$filename = basename($post->guid);
+	$title = esc_attr($post->post_title);
+
+	if ( $_tags = get_the_tags($attachment_id) ) {
+		foreach ( $_tags as $tag )
+			$tags[] = $tag->name;
+		$tags = esc_attr(join(', ', $tags));
+	}
+
+	$post_mime_types = get_post_mime_types();
+	$keys = array_keys(wp_match_mime_types(array_keys($post_mime_types), $post->post_mime_type));
+	$type = array_shift($keys);
+	$type_html = "<input type='hidden' id='type-of-$attachment_id' value='" . esc_attr( $type ) . "' />";
+
+	$form_fields = get_attachment_fields_to_edit($post, $errors);
+
+	if ( $toggle ) {
+		$class = empty($errors) ? 'startclosed' : 'startopen';
+		$toggle_links = "
+	<a class='toggle describe-toggle-on' href='#'>$toggle_on</a>
+	<a class='toggle describe-toggle-off' href='#'>$toggle_off</a>";
+	} else {
+		$class = 'form-table';
+		$toggle_links = '';
+	}
+
+	$display_title = ( !empty( $title ) ) ? $title : $filename; // $title shouldn't ever be empty, but just in case
+	$display_title = $show_title ? "<div class='filename new'><span class='title'>" . wp_html_excerpt($display_title, 60) . "</span></div>" : '';
+
+	$gallery = ( (isset($_REQUEST['tab']) && 'gallery' == $_REQUEST['tab']) || (isset($redir_tab) && 'gallery' == $redir_tab) ) ? true : false;
+	$order = '';
+
+  $playlist_items = explode(",", get_post_meta($current_playlist, LONGTAIL_KEY. "playlist_items", true));
+
+  $checked = "";
+  foreach ($playlist_items as $playlist_item) {
+    if ($playlist_item == $attachment_id) {
+      $checked = "checked='true'";
+      break;
+    }
+  }
+
+	foreach ( $form_fields as $key => $val ) {
+		if ( 'menu_order' == $key ) {
+			if ( true ) {
+				$order = '<div class="menu_order">';
+        $order .= '<input class="menu_order_input" type="checkbox" id="attachments['.$attachment_id.'][enabled]" name="attachments['.$attachment_id.'][enabled]" value="'.$val['value'].'"'.$checked.' /></div>';
+      } else
+				$order = '<input type="hidden" name="attachments['.$attachment_id.'][menu_order]" value="'.$val['value'].'" />';
+
+			unset($form_fields['menu_order']);
+			break;
+		}
+	}
+
+	$media_dims = '';
+	$meta = wp_get_attachment_metadata($post->ID);
+	if ( is_array($meta) && array_key_exists('width', $meta) && array_key_exists('height', $meta) )
+		$media_dims .= "<span id='playlist-dims-{$post->ID}'>{$meta['width']}&nbsp;&times;&nbsp;{$meta['height']}</span> ";
+	$media_dims = apply_filters('media_meta', $media_dims, $post);
+
+	$image_edit_button = '';
+	if ( gd_edit_image_support($post->post_mime_type) ) {
+		$nonce = wp_create_nonce("image_editor-$post->ID");
+		$image_edit_button = "<input type='button' id='imgedit-open-btn-{$post->ID}' onclick='imageEdit.open($post->ID, \"$nonce\")' class='button' value='" . esc_attr__( 'Edit image' ) . "' /> <img src='images/wpspin_light.gif' class='imgedit-wait-spin' alt='' />";
+	}
+
+	$item = "
+	$type_html
+	$toggle_links
+	$order
+	$display_title
+	<table class='slidetoggle describe $class'>
+		<thead class='playlist-item-info' id='playlist-head-$post->ID'>
 		<tr>
 			<td class='A1B1' id='thumbnail-head-$post->ID' rowspan='5'><img class='thumbnail' src='$thumb_url' alt='' /></td>
 			<td><strong>" . __('File name:') . "</strong> $filename</td>
